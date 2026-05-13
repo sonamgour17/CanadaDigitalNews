cedf6c8117d252372c710ff72ac9cb604b1a2484
Sonam Gour <sonamgour17@gmail.com>
2026-05-13 16:41:21 -0400

    Initial Commit

---
+++ CanadaDigitalNews/CanadaDigitalNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanadaDigitalNewsApp.swift
+//  CanadaDigitalNews
+//
+//  Created by Sonam Gour on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CanadaDigitalNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanadaDigitalNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CanadaDigitalNews
+//
+//  Created by Sonam Gour on 13/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
